3cb452442298a0f916f8a0774aaae69af7c663f5
Indraja <induponimoni542@gmail.com>
2025-03-21 21:26:45 +0530

    Lru-dsa

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use(expressLayouts);
 app.set("layout", "layouts/boilerplate");
 
 const port =process.env.PORT || 3000;
+app.get("/", (req, res) => {
+    res.send("LRU Cache Visualizer running!");
+  });
 
 app.listen(port, () => {
     console.log("Server running on port", port);
